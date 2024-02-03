bbe8e98d8314deed94dd7ed7b3cc243a20c20f81
hanep <yudishan26@gmail.com>
2024-02-03 20:09:48 +0700

    create hero section

---
+++ app/components/Navbar.tsx
@@ -0,0 +1,19 @@
+import Link from "next/link";
+
+export default function Navbar() {
+    return (
+        <nav className="absolute top-0 w-full h-28 text-slate-100">
+            <div className="max-w-screen-xl px-4 flex justify-between h-full items-center m-auto">
+                <div>
+                    <h3 className="text-2xl font-bold tracking-widest">IiIiIiI</h3>
+                </div>
+                <div className="flex gap-5 text-lg">
+                    <Link href="#home" className="hover:border-b hover:border-orange-700">Home</Link>
+                    <Link href="#home" className="hover:border-b hover:border-orange-700">About</Link>
+                    <Link href="#home" className="hover:border-b hover:border-orange-700">Contact</Link>
+                </div>
+                <button className="border-2 border-orange-800 px-4 py-1 rounded-full hover:bg-orange-800">Let&#39;s talk</button>
+            </div>
+        </nav>
+    )
+}
