306afdd54b28a0b0df5bf8849f6a930dda442ffd
hanep <yudishan26@gmail.com>
2024-02-05 19:53:15 +0700

    update hero section

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -2,7 +2,7 @@ import Link from "next/link";
 
 export default function Navbar() {
     return (
-        <nav className="absolute top-0 w-full h-28 text-slate-100">
+        <nav className="absolute top-0 w-full h-28 text-slate-100 z-10">
             <div className="max-w-screen-xl px-4 flex justify-between h-full items-center m-auto">
                 <div>
                     <h3 className="text-2xl font-bold tracking-widest">IiIiIiI</h3>
@@ -12,7 +12,7 @@ export default function Navbar() {
                     <Link href="#home" className="hover:border-b hover:border-orange-700">About</Link>
                     <Link href="#home" className="hover:border-b hover:border-orange-700">Contact</Link>
                 </div>
-                <button className="border-2 border-orange-800 px-4 py-1 rounded-full hover:bg-orange-800">Let&#39;s talk</button>
+                <button className="border-2 border-orange-700 px-4 py-1 rounded-full hover:bg-orange-700">Let&#39;s talk</button>
             </div>
         </nav>
     )
