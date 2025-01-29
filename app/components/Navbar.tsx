65f2e94b055fdc5aa10b7f7e24b559126e63d473
hanep77 <yudishan26@gmail.com>
2025-01-29 19:49:20 +0700

    adding responsiveness

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,20 +1,61 @@
+"use client";
+
 import Link from "next/link";
+import { useEffect, useState } from "react";
 
 export default function Navbar() {
+  const [sidebar, setSidebar] = useState<boolean>(false)
+  const [isScrolled, setIsScrolled] = useState<boolean>(false)
+
+  function handleSidebar() {
+    setSidebar(!sidebar)
+  }
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY > 0) {
+        setIsScrolled(true);
+      } else {
+        setIsScrolled(false);
+      }
+    }
+
+    window.addEventListener("scroll", handleScroll);
+    return () => window.addEventListener("scroll", handleScroll);
+  }, []);
+
   return (
-    <nav className="absolute w-full h-16 z-10">
+    <nav className={`fixed ${sidebar && "bg-zinc-200"} ${isScrolled && "bg-zinc-200 shadow"} w-full h-16 z-10`}>
       <div className="max-w-screen-lg px-4 xl:px-0 flex justify-between h-full items-center m-auto">
         <div>
           <h3 className="text-2xl font-bold tracking-widest">IiIiIiI</h3>
         </div>
-        <div className="flex font-medium">
-          <Link href="#home" className="hover:text-white hover:bg-orange-700 rounded-full px-4 py-1">Home</Link>
-          <Link href="#home" className="hover:text-white hover:bg-orange-700 rounded-full px-4 py-1">About</Link>
-          <Link href="#home" className="hover:text-white hover:bg-orange-700 rounded-full px-4 py-1">Service</Link>
-          <Link href="#home" className="hover:text-white hover:bg-orange-700 rounded-full px-4 py-1">Contact</Link>
-          <Link href="#home" className="hover:text-white hover:bg-orange-700 rounded-full px-4 py-1">FAQ</Link>
+        <div className={`bg-zinc-200 md:bg-transparent fixed md:static z-30 mt-16 top-0 left-0 bottom-0 right-0 md:mt-0 ${!sidebar && 'translate-x-full'} transition md:translate-x-0 flex`}>
+          <ul className="flex flex-col md:flex-row items-center gap-5 md:gap-0 text-lg font-medium px-4 md:px-0 bg-red w-full">
+            <li className="w-full md:w-auto text-center md:text-start">
+              <a href="#" className="hover:text-white hover:bg-orange-600 rounded-full px-4 py-1" onClick={handleSidebar}>Home</a>
+            </li>
+            <li className="w-full md:w-auto text-center md:text-start">
+              <a href="#about" className="hover:text-white hover:bg-orange-600 rounded-full px-4 py-1" onClick={handleSidebar}>About</a>
+            </li>
+            <li className="w-full md:w-auto text-center md:text-start">
+              <a href="#services" className="hover:text-white hover:bg-orange-600 rounded-full px-4 py-1" onClick={handleSidebar}>Services</a>
+            </li>
+            <li className="w-full md:w-auto text-center md:text-start">
+              <a href="#faq" className="hover:text-white hover:bg-orange-600 rounded-full px-4 py-1" onClick={handleSidebar}>FAQ</a>
+            </li>
+            <li className="w-full md:w-auto text-center md:text-start">
+              <a href="#contact" className="hover:text-white hover:bg-orange-600 rounded-full px-4 py-1" onClick={handleSidebar}>Contact</a>
+            </li>
+          </ul>
         </div>
-        <button className="bg-orange-600 px-4 py-2 font-medium rounded-full text-zinc-200 hover:bg-orange-700">Let&#39;s talk</button>
+        <div className="flex md:hidden gap-4">
+          <button className={`flex flex-col gap-[6px] ${sidebar && 'translate-y-1'}`} onClick={handleSidebar}>
+            <div className={`w-8 h-[3px] bg-orange-600 rounded ${sidebar && 'translate-y-[6px] -translate-x-[4px] rotate-45'} transition`}></div>
+            <div className={`w-8 h-[3px] bg-orange-600 rounded m-auto ${sidebar && 'w-0'} transition-all`}></div>
+            <div className={`w-8 h-[3px] bg-orange-600 rounded ${sidebar && '-translate-y-[12px] -translate-x-[4px] -rotate-45'} transition`}></div>
+          </button>
+        </div>
       </div>
     </nav>
   )
--- app/sections/Hero.tsx
+++ app/sections/Hero.tsx
@@ -1,15 +1,15 @@
 import Image from "next/image";
 
 export default function Hero() {
-  return <section id="home" className="relative px-4 xl:px-0" style={{ backgroundImage: "url(/assets/architect.svg)" }}>
-    <div className="max-w-screen-lg m-auto">
-      <div className="w-1/2 min-h-screen 2xl:min-h-[768px] flex flex-col justify-center items-center">
-        <h1 className={`text-7xl font-extrabold max-w-screen-md leading-[80px]`}>Bringing <span className="text-orange-600">Elegance </span>to Every <span className="text-orange-600">Corner</span></h1>
+  return <section id="home" className="relative flex flex-col md:block" style={{ backgroundImage: "url(/assets/architect.svg)" }}>
+    <div className="max-w-screen-lg m-auto px-4 xl:px-0 order-2">
+      <div className="md:w-1/2 md:min-h-screen 2xl:min-h-[768px] flex flex-col md:justify-center items-center mt-5 md:mt-0">
+        <h1 className={`text-6xl md:text-7xl font-extrabold max-w-screen-md md:leading-[80px]`}>Bringing <span className="text-orange-600">Elegance </span>to Every <span className="text-orange-600">Corner</span></h1>
         <p className="pt-5 text-lg text-zinc-600">We bring your vision to life with innovative designs that combine style, functionality, and comfort.</p>
       </div>
     </div>
-    <div className="absolute inset-0 flex items-center">
-      <div className="absolute bg-zinc-300 backdrop-blur-sm bg-opacity-15 shadow h-4/6 rounded-s-xl w-1/2 right-0 p-4 pe-0">
+    <div className="md:absolute inset-0 flex items-center ps-4 md:ps-0 md:mt-0 pt-20 md:pt-0">
+      <div className="md:absolute bg-zinc-300 backdrop-blur-sm bg-opacity-15 shadow h-1/2 md:h-4/6 rounded-s-xl md:w-1/2 w-full right-0 p-4 pe-0">
         <Image src={"/assets/house-3.jpg"} alt="room" width={1280} height={768} className="h-full object-cover rounded-s-md" />
       </div>
     </div>
